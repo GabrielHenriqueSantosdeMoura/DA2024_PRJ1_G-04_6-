03e1a4fa23e104e493f2c5acffc885cf0b6781f0
Sofia Blyzniuk <CTW02617-admin@L-PF38LPFZ.criticaltechworks.com>
2024-03-20 01:46:09 +0000

    Implemented adding vertexes to the graph

--- Code/headers/DataReader.h
+++ Code/headers/DataReader.h
@@ -14,10 +14,19 @@ using namespace std;
 class DataReader{
     //Graph of a watter supply system
     Graph <WaterSystem> supplySystem;
+
 public:
 
-DataReader(const string &citiesMadeira, const string &pipesMadeira, const string &reservoirsMadeira ,const string & stationsMadeira);
+    DataReader(const string &citiesMadeira, const string &pipesMadeira, const string &reservoirsMadeira ,const string & stationsMadeira);
 
-}
+    void readCities(const string& citiesMadeira, WaterSystem& waterSystem);
+
+    void readPipes(const string& pipesMadeira, WaterSystem& waterSystem);
+
+    void readReservoirs(const string& reservoirsMadeira, WaterSystem& waterSystem);
+
+
+    void readPumpingStations(const string& stationsMadeira, WaterSystem& waterSystem);
+};
 
 #endif //DA2024_PRJ1_G_04_6_READDATA_H
--- Code/src/DataReader.cpp
+++ Code/src/DataReader.cpp
@@ -10,15 +10,28 @@ using namespace std;
 //the class will add the Vertices of a Graph
 DataReader::DataReader(const string &citiesMadeira, const string &pipesMadeira,const string &reservoirsMadeira, const string &stationsMadeira)
 {
-    //Create an instance of Water System class
+    /// Create an instance of WaterSystem class
     WaterSystem waterSystem;
-    //class to read the data and represent as a graph
+
+    readCities(citiesMadeira, waterSystem);
+
+    readPipes(pipesMadeira, waterSystem);
+
+    readReservoirs(reservoirsMadeira, waterSystem);
+
+    readPumpingStations(stationsMadeira, waterSystem);
+
+}
+
+void DataReader::readCities(const string& citiesMadeira, WaterSystem& waterSystem) {
+    //stream to read the Cities_Madeira file and represent as a graph
     ifstream citiesMadeira_file(citiesMadeira);
 
     //check if the file exists
     if (!citiesMadeira_file.is_open()){
         throw runtime_error("Cities Madeira file does not exist");
     }
+
     string line;
 
     while(getline(citiesMadeira_file,line)){
@@ -31,5 +44,71 @@ DataReader::DataReader(const string &citiesMadeira, const string &pipesMadeira,c
         City city(name,idValue,code,demandValue,population);
         waterSystem.addCity(city);
     }
+}
 
+void DataReader::readPipes(const string& pipesMadeira, WaterSystem& waterSystem) {
+    //stream to read the Cities_Madeira file and represent as a graph
+    ifstream pipesMadeira_file(pipesMadeira);
+
+    //check if the file exists
+    if (!pipesMadeira_file.is_open()){
+        throw runtime_error("Cities Madeira file does not exist");
+    }
+
+    string line;
+
+    while(getline(pipesMadeira_file,line)){
+        istringstream iss(line);
+        string sourceService,targetService,capacity,direction;
+        getline(getline(getline(getline(iss, sourceService, ','), targetService, ','), capacity, ','), direction,'\r');
+        double capacityValue = stoi(capacity);
+        bool directionValue = stoi (direction);
+
+        Pipe pipe(sourceService,targetService,capacityValue,directionValue);
+        waterSystem.addPipe(pipe);
+    }
+}
+
+void DataReader::readReservoirs(const string& reservoirsMadeira, WaterSystem &waterSystem) {
+    //stream to read the Cities_Madeira file and represent as a graph
+    ifstream reservoirsMadeira_file(reservoirsMadeira);
+
+    //check if the file exists
+    if (!reservoirsMadeira_file.is_open()){
+        throw runtime_error("Cities Madeira file does not exist");
+    }
+
+    string line;
+
+    while(getline(reservoirsMadeira_file,line)){
+        istringstream iss(line);
+        string name,municipality,id,code,maxDelivery;
+        getline(getline(getline(getline(getline(iss, name, ','), municipality, ','), id, ','), code, ','), maxDelivery, '\r');
+        int idValue = stoi(id);
+        double maxDeliveryValue = stoi (maxDelivery);
+
+        Reservoir reservoir(name,municipality,idValue,code,maxDeliveryValue);
+        waterSystem.addReservoir(reservoir);
+    }
+}
+
+void DataReader::readPumpingStations(const std::string &stationsMadeira, WaterSystem &waterSystem) {
+    //stream to read the Cities_Madeira file and represent as a graph
+    ifstream stationMadeira_file(stationsMadeira);
+
+    //check if the file exists
+    if (!stationMadeira_file.is_open()){
+        throw runtime_error("Cities Madeira file does not exist");
+    }
+
+    string line;
+
+    while(getline(stationMadeira_file, line)){
+        istringstream iss(line);
+        string id, code;
+        getline(getline(iss, id, ','), code, '\r');
+        int idValue = stoi(id);
+        PumpingStation pumpingStation(idValue, code);
+        waterSystem.addPumpingStation(pumpingStation);
+    }
 }
